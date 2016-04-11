cfc1c628e3f662901a5c0dfbe7a931207accd377
Hintay <hintay@me.com>
2016-04-11 13:17:30 +0800

    Initial version

---
+++ macroslist.py
@@ -0,0 +1,300 @@
+macros = {
+	'BFNT':'@font',
+	'BIED':'@interlude_end',
+	'BIVF':{'0':'@visibleframe', '1':'@invisibleframe'},
+	'BIST':'@interlude_start',
+	'BIED':'@interlude_end',
+	'BRET':'@return',
+	'BTXO':{'0':'@textoff', '1':'@texton'},
+	'CFAD':{'0':'@fadein', '2':'@rep'},
+	'CHCL':{'0':{'0':'@condoff', '2':'@red', '3':'@green', '4':'@blue', '5':'@nega', '6':'@monocro'}, '1':{'2':'@red', '8':'@sepia'}},
+	'CHZB':'@haze_back',
+	'CHZS':'@stophaze',
+	'CHTC':{'1':'@hearttonecombo'},
+	#'COND':'',
+	'CTRX':'@transex',
+	'CCTO':{'0':'@contrastoff'},
+	'CCTR':{'1':'@contrast'},
+	'FCAL':'@call', #_FCAL((1166; @call storage=街編・1日目-03.ks
+	'HRDW':'@redraw',
+	'HFAN':'@hfangry',
+	'HFBB':'@hfburstblood',
+	'HFCN':'@hfchance',
+	'HFFA':'hfface',
+	'HFFC':'@hffacechg',
+	'HFFG':'@hffeelgood',
+	'HFSG':'@hfsigh',
+	'HFST':'@hfstamp',
+	'HFSW':'@hfsweat',
+	'HFPP':'@hfpop',
+	'HFWW':'@hfwww',
+	#'HFTF':'@tf',
+	#'HFUL':{'2':''}, #_HFUL(2; [hfu]字[hfl] 须特殊处理
+	'HSMG':{'0':'@smudge'},
+	'KDSH':'@dash',
+	'KDSS':{'0':'@stopdash'},
+	'KDSW':'@wdash',
+	'KFCH':'@chgfg',
+	'KFG0':'@fg',
+	'KFMV':'@movefg', # @monocro
+	'KMST':'@stopmove', # @stopsplinemove
+	'KMWT':'@wm',
+	#'KMW2':'',
+	'KMVE':'@move',
+	'KMLP':'@loopmove',
+	'KSMW':'@wsplinemove',
+	'KHZE':'@haze',
+	'KFND':'@find',
+	'KSMV':'@splinemove',
+	'KIMG':'@image',
+	'MPLY':{'0':'@play', '2':'@xchgbgm'},
+	'MPAU':{'0':'@playresume', '1':'@playpause'},
+	'MSTP':{'0':'@playstop'},
+	'NEVL':'@eval', #_NEVL(exp=Scripts.execStorage(HanafudaPlugin.tjs);
+	'NCL0':'@cl',
+	'NCLF':'@clfg',
+	'NCIN':{'0':'@cinesco'},
+	'NCTR':'@cltransparent',
+	'NNOB':'@noise_back',
+	'NNOI':{'0':'@noise'},
+	'NNOS':{'0':'@stopnoise'}, # @noise_off
+	'NQUS':'@stopquake',
+	'NQUL':'@lquake',
+	'NQLS':'@stoplquake',
+	'NSHK':{'0':'@shock'},
+	'PAGE':'*page',
+	#'PEND':'', #_PEND(; 占位符？
+	#'QSET':{'0':'@call storage=QuizSystem.ks\n@iscript'},
+	'QADE':'@endscript',
+	'QADS':'.quiz	= %[%s]',
+	'QADD':'quiz:[%s]',
+	'SESF':'@sestop',
+	'SEPF':{'0':'@se', '2':'@seloop', '0tdeepdaytime':'@setdeepdaytime', '0thscene':'@sethscene'},
+	'SEFF':'@fadese',
+	'SNOW':{'0':'@snowuninit', '1':{'800':'@snowinit forevisible=false backvisible=true'}}, #@snowopt backvisible=false
+	'TDSP':{'0':'@displayedoff', '1':'@displayedon'},
+	'TPG0':'@pg',
+	'TWND':{'0':'@setdaytime', '1':'@setnighttime'},
+	'WTFT':{'0':'@wait', '0acanskip=false':'@wait acanskip=false'},
+	'WTKY':'@lr',
+	'WNDS':{'0':'@window_start', '1':'@window_end', '2':''},
+	'MFNR':'@resetfont', # and @rf
+	#'SYNC;:'@starttag',
+	#'WKST':'',
+	#'MVPL':{2:'@playmovie'},
+	#'MSAD':'', #文本框
+}
+# @smudge @blur @smudgeoff @bluroff @slideopencombo @slideclosecombo @pasttime 被_CFAD所替代
+# @shortcutkey @history 
+#_WKST(G054,1; @night_start _WKST(G011,1; 真・冒頭-16.ini
+
+# 以`开头
+parameters = {
+	#'002':'haverule', # 2=on 0=none ?
+	'003':'time',
+	'004':'vague',
+	'005':'storage', # bg for @rep?
+	'008':'canskip',
+	'010':'page',
+	'012':'layer', # -2=&no -1=base 255=all
+	'013':'left',
+	'014':'top',
+	'015':'opacity',
+	'017':'accel',
+	'018':'cx',
+	'019':'cy',
+	'020':'imag',
+	'021':'mag',
+	'023':'range', #_HSMG
+	'024':'level',
+	#'026':'tf', #_HFTF ??
+	'032':'pos',
+	'033':'index',
+	'034':'noclear',
+	'035':'fliplr',
+	'036':'flipud',
+	'037':'poss',
+	'039':'indexes',
+	'040':'opacities',
+	'041':'vmax',
+	'042':'count',
+	'043':'delay',
+	'044':'type',
+	'045':'nowait',
+	'046':'textoff',
+	'047':'irot',
+	'048':'mx',
+	'049':'my',
+	'050':'rot',
+	'051':'face',
+	'067':'hmax',
+	'068':'upper',
+	'069':'lower',
+	'074':'volume',
+	'075':'target',
+	'076':'hidefg',
+	'086':'color',
+	'087':'storages',
+	'089':'center',
+	'090':'upperpow',
+	'091':'lowerpow',
+	'092':'centerpow',
+	#'094':'id', #_SYNC starttag
+	'096':'lwaves',
+	'098':'base',
+	'099':'px',
+	'100':'py',
+	'101':'deg',
+	'111':'overlap',
+	#'113':'', #_KDSH @dash
+	'114':'monocro', 
+	'115':'both', #_KFMV @movefg
+	'116':'mover', #_KMLP @loopmove
+	'117':'frame',
+	'118':'decel',
+	'119':'spread', #_KMVE @move
+	'121':'intime',
+	'122':'waves',
+	'124':'standard',
+	'128':'edgecolor',
+	'131':'italic',
+	#'139':'bg', #? _KDSH @dash _COND
+	'141':'indexs',
+	'164':'rule',
+	'170':'spline',
+	'171':'affine',
+	'172':'path',
+	'216':'last',
+	'217':'lv2off',
+	'218':'fliplrs',
+	'219':'flipuds',
+	'220':'avoid',
+	'221':'force',
+	'222':'layers',
+	'223':'lefts',
+	'224':'tops',
+	'227':'colors',
+	'228':'monos',
+	'229':'mono',
+	'230':'noquake',
+	'231':'bluroff',
+	'232':'chara',
+	'233':'question',
+	'234':'alters',
+	#'235':'result',
+	'236':'nospline',
+	#'237':'', #@loopmove
+	#'238':'', #@chgfg
+	#'239':'', #@loopmove
+}
+
+target = {
+	'1':'bg',
+	'2':'fg',
+	'3':'all',
+}
+
+page = {
+	'0':'back',
+	'1':'fore'
+}
+
+call = {
+	'(1013':'カレン-01.ks',
+	'(1166':'街編・1日目-03.ks',
+	'(1179':'街編・1日目-24.ks'
+}
+
+rule = {
+	'001':'crystal_bt',
+	'002':'l2r_half',
+	'003':'l2r_ss',
+	'004':'mosaic_lt_rb',
+	'005':'pageleft',
+	'006':'pageright',
+	'007':'r2l_half',
+	'008':'r2l_sfs',
+	'009':'r2l_ss',
+	'010':'sparm',
+	'011':'trans000',
+	'012':'カーテン左から',
+	'013':'カーテン上から',
+	'014':'シャッター下から',
+	'015':'シャッター左から',
+	'016':'シャッター上から',
+	'022':'モザイク',
+	'027':'やや細かい縦ブラインド(中央から左右へ)',
+	'028':'右から左へ',
+	'029':'右渦巻き',
+	'030':'右下から左上へ',
+	'031':'右上から左下へ',
+	'032':'円形(外から中へ)',
+	'033':'円形(中から外へ)',
+	'036':'下から上へ',
+	'037':'左から右へ',
+	'038':'左下から右上へ',
+	'039':'左回り',
+	'041':'左上から右下へ',
+	'048':'斜めチェッカー',
+	'052':'上から下へ',
+	'053':'走る感じ(右から)',
+	'054':'走る感じ(下から)',
+	'055':'走る感じ(上から)',
+	'056':'走る感じ',
+	'058':'短冊(下から)',
+	'059':'短冊(上から)',
+	'060':'短冊細(右から)',
+	'061':'短冊細(左から)',
+	'062':'虫食い',
+	'063':'波',
+	'064':'放射状(時計回り)',
+	'066':'koyama02r',
+	'067':'カレン割',
+	'070':'crystal_bt_r',
+	'071':'forfd05',
+	'072':'forfd05_2',
+	'073':'forfd逆月07',
+	'074':'forRider01',
+	'075':'koyama01r',
+	'076':'koyama02r2',
+	'078':'中央から左右へ',
+	'079':'左回り連続2',
+}
+
+pos = {
+	'-1':'all',
+	'1':'l',
+	'2':'r',
+	'3':'lc',
+	'4':'rc',
+	'5':'c',
+	'1ower':'lower', #替换文本 
+}
+
+layer = {
+	'-2':'&no',
+	'-1':'base',
+	'255':'all'
+}
+
+special_macro = { 'target':target, 'page':page, 'call':call, 'rule':rule, 'pos':pos, 'layer':layer }
+
+ignore_macro = ['SYNC', 'KMW2']
+
+# 有括号
+brackets_macro = ['BTXO', 'BIVF', 'MPAU']
+
+# 无逗号
+no_comma_macro = ['KHZE', 'PAGE', 'FCAL', 'NEVL', 'KFCH']
+
+# _ZM4be03( [block len=9] 0x8145 _ZM数字可对应为 ;数字
+# @s(28) @large @s(16) @small
+# MSAD 对应
+# ^ = 换行 ^^ = 换行+@r 行中为[br]
+# @a(id) = call _SYNC
+# @c(0,0,0) = [font color=0x000000]
+# @n 包含下一行的macro
+# <瀕,ひん> = [ruby text=ひん]瀕
+# _QJMP(_D990,quiz14_correct,quiz14_incorrect; @quiz success=*page11 failed=*page12
+# _HFTF(0,`026:クイズタイガー編クリア,`235:1; @eval exp="tf['クイズタイガー編クリア']=true"
+# [wacky len=3] [block 0X8145
---
+++ psvstool.py
@@ -0,0 +1,386 @@
+from macroslist import *
+import os
+import re
+import sys
+import codecs
+import argparse
+
+encoding = 'Shift_JIS'
+
+# 处理行
+class ScenarioLine:
+	def __init__(self, line):
+		self.line = line
+		self.newline = ''
+		self.macro = None
+		self.macro_type = None
+		self.macro_subtype = None
+		self.parameters = ''
+		self.newparameters = ''
+		self.matchs = None
+		self.ismessage = False
+		self.messageid = None
+		self.match_line()
+
+	# 正则匹配提取数据
+	def match_line(self):
+		self.matchs = re.match(b'^_(.*?)\((.*)', self.line)
+		if(self.matchs): # 匹配至(宏)(宏参数)
+			self.macro = self.matchs.group(1).decode()
+			# 占位 - 判断语音
+
+			if(self.macro == 'HFUL'):
+				return
+			# 判断文本
+			elif(self.macro == 'MSAD' or self.macro == 'MSA2'):
+				self.ismessage = True
+				self.parameters = self.matchs.group(2)
+				return
+			elif(self.macro[:2] == 'ZM' or self.macro[:2] == 'ZZ'):
+				self.ismessage = True
+				self.parameters = self.matchs.group(2)
+				self.messageid = self.macro[2:] # ID
+				return
+			# /判断文本
+
+			elif(self.macro in ignore_macro):
+				return
+			elif(self.macro in macros.keys()): # macro 列表中有匹配
+				if self.matchs.group(2): # 若有参数
+					self.parameters = self.matchs.group(2).split(bytes(',`', encoding)) # 分割后[0]为宏参数
+			#else: #找不到相应的macro
+		else:
+			# 没匹配到任何数据
+			if not self.line == b'':
+				print(self.line)
+			else: # 空行
+				return
+
+		self.get_macro()
+
+	# 匹配宏类型
+	def match_macro_type(self):
+		if not self.parameters or self.ismessage or self.macro in no_comma_macro:
+			return
+		parameter = self.parameters[0]
+		if self.macro in brackets_macro and parameter[-1:] == b')':
+			parameter = parameter[:-1]
+		macro_types = parameter.decode(encoding).split(',')
+		self.macro_type = macro_types[0]
+		if len(macro_types) > 1: # 宏子类型
+			if self.macro == 'BTXO': # 特殊处理，无额外参数的
+				self.parameters.append(b'003:'+macro_types[1].encode(encoding))
+			else:
+				self.macro_subtype = macro_types[1]
+		try: # 有对应macro则删除参数
+			touch = macros[self.macro][self.macro_type]
+			del self.parameters[0] # 使用后移除参数
+		# 无对应macro，例：*page
+		except TypeError:
+			touch = ''
+		except KeyError:
+			touch = ''
+
+	# 获取宏名字
+	def get_macro_name(self):
+		if( self.macro and self.macro in macros.keys()):
+			#print(self.macro)
+			#print(self.macro_type)
+			if self.macro_type != None and self.macro_type != '':
+				if self.macro_subtype != None and self.macro_subtype != '':
+					return(macros[self.macro][self.macro_type][self.macro_subtype])
+				else:
+					return(macros[self.macro][self.macro_type])
+			else:
+				return(macros[self.macro])
+		else:
+			return # 无对应的macro type
+
+	# 获取参数文本
+	def get_parameters(self):
+		for par in self.parameters:
+			parsplit = par.decode(encoding).split(':')
+			# 若分割出的参数小于2则没有参数可分割
+			if not len(parsplit) < 2:
+				if(parsplit[0] in parameters.keys()):
+					# 特殊对应参数
+					if parameters[parsplit[0]] in special_macro.keys():
+						if parsplit[1] in special_macro[parameters[parsplit[0]]].keys():
+							self.newparameters += ' %s=%s' % (parameters[parsplit[0]], special_macro[parameters[parsplit[0]]][parsplit[1]])
+							continue
+					# /特殊对应参数
+					self.newparameters += ' %s=%s' % (parameters[parsplit[0]], parsplit[1])
+				else: # 没有相应的参数
+					if parsplit[0] == '002':
+						continue
+					self.newparameters += ' `%s=%s' % (parsplit[0], parsplit[1])
+			else:
+				if parsplit[0] == 'extoff=0': # 修复错误
+					self.newparameters += ' textoff=0'
+				elif parsplit[0]: self.newparameters += ' ' + parsplit[0]
+
+	def get_macro(self):
+		if self.ismessage: # 文本内容直接退出
+			return
+		self.match_macro_type()
+		macro = self.get_macro_name()
+		if(macro): # 有对应的 macro 名
+			self.get_parameters() # 格式化参数
+			self.newline += macro
+			self.newline += self.newparameters
+		else:
+			self.newline += ';%s' % self.line.decode(encoding)
+
+# 处理剧本Message
+class ScenarioMessage:
+	def __init__(self):
+		self.need_next = False
+		self.temp_need_next = 0 # @n 临时下一行 搜索有几个@n就重复几次
+		self.WTVT_macro = False
+		self.is_HFUL = False
+		self.need_handle_line = None
+		self.inline_macros = '' # 转换好的macros
+		self.newlines = ''
+
+	# 重置所有参数
+	def reset_variables(self):
+		self.__init__()
+
+	# 添加一行
+	def add_line(self, lineinfo):
+		if lineinfo.ismessage: # 若为 Message
+			# 特殊情况重置
+			if self.temp_need_next > 0 or self.WTVT_macro == True:
+				self.end_inline_macros('n')
+			# 若需要添加下一行但本行又为文本的话则封闭 Macro 再处理本行
+			if self.need_next:
+				self.end_inline_macros('a')
+			# /特殊情况重置
+
+			if lineinfo.messageid: # 有 ID 则加上 ID 注释
+				self.newlines += ';%d, %s\n' % (int(lineinfo.messageid, 16), lineinfo.messageid)
+
+			if lineinfo.parameters: # 内容处理
+				self.handle_message_line(lineinfo.parameters)
+		else:
+			# 将 Macro 的 @ 替换为 [
+			if lineinfo.newline[:1] == '@':
+				self.inline_macros += '[' + lineinfo.newline[1:] +']'
+			elif not lineinfo.newline == '':
+				self.inline_macros += '\n' + lineinfo.newline + '\n'
+
+			# 行内 @n 要包含的 Macro 块也在这里处理
+			if self.temp_need_next > 0 and self.WTVT_macro == False:
+				# 语音等待符号
+				if lineinfo.macro == 'WTVT':
+					self.WTVT_macro = True
+					return
+				self.temp_need_next -= 1
+				if self.temp_need_next == 0:
+					self.end_inline_macros('n')
+
+	# 处理Message
+	def handle_message_line(self, content):
+		# 先匹配特殊字符 注意：返回值为 Unicode 字符串
+		newlines = self.handle_special_character(content)
+
+		# [hfu] [hfl]
+		if(self.is_HFUL):
+			self.is_HFUL = False
+			newlines = self.handle_hful_macro(newlines)
+
+		# 处理行内的 Marco： @n 或 @a(id)
+		# ！注意！ @a 与 @n 会出现在同一行！@a 比 @n 先出现
+		# ！注意！ @a 可能会多次出现！
+		# 
+		# @a(ID) ID与 _SYNC( 中 ID 相同
+		# 不用管有 @a 行中的 @n
+		if(newlines.find('@a') >= 0):
+			if newlines[-2:] == '@n': newlines = newlines[:-2]
+
+			self.need_handle_line = newlines
+			self.need_next = True
+			return
+		# @n 重复几次即为包含下面几行
+		n_count = newlines.count(r'@n')
+		if(n_count):
+			self.need_handle_line = newlines
+			self.temp_need_next = n_count
+		# 有需要处理的行代表暂时不能直接返回
+		if self.need_handle_line:
+			return
+		# /处理行内的 Marco
+
+		self.newlines += newlines
+
+	def handle_hful_macro(self, content):
+		new_content = ''
+		for i in range(len(content)):
+			if content[i] == '。': continue
+			try:
+				char = (content[i] + content[i+1]) if content[i+1] == '。' else content[i]
+			except IndexError:
+				char = content[i]
+			new_content += '[hfu]' + char + '[hfl]' + char
+		return new_content
+
+	# 行内 Macros 结束
+	def end_inline_macros(self, type):
+		if type == 'a':
+			# 重置 Flag
+			self.need_next = False
+			# 正则替换行内的@a(ID)
+			self.newlines += re.sub('(@a\(\d+\))', self.inline_macros, self.need_handle_line)
+		else:
+			self.temp_need_next = 0
+			self.WTVT_macro = False
+			# 正则替换行内的@n
+			self.newlines += re.sub('(@n)+', self.inline_macros, self.need_handle_line)
+		# 重置部分参数
+		self.need_handle_line = None
+		self.inline_macros = ''
+
+	# 处理特殊字符 (基本以 \xec 开头)
+	# \xec\x4a = Line Macro Start
+	# \xec\x46 = Line Macro Padding Character
+	# \xec\x48 = Line Macro End
+	# \xec\x5a = [argz]
+	# \xec\x5c = [nusz]
+	# \xec\x59 = [ansz]
+	# \xec\x5b = [ingz]
+	# \xec\x45 = [heart]
+	# \xec\x5d = Append Macro End Tag 行内macro结束标记
+	# \xec\x4b = Block Macro Start
+	# \xec\x47 = Block Macro Padding Character
+	# \xec\x49 = Block Macro End
+	# \xec\x50 = Wacky Macro Length 3
+	# \xec\x52 = Wacky Macro Length 6
+	# \xec\x53 = Wacky Macro Length 9
+	# \xec\x54 = Wacky Macro Length 12
+	def handle_special_character(self, content):
+		content = content.replace(b'\xec\x5d', b'') # 直接去掉结束标记
+		content = content.replace(b'\xec\x5a', b'[argz]')
+		content = content.replace(b'\xec\x5c', b'[nusz]')
+		content = content.replace(b'\xec\x59', b'[ansz]')
+		content = content.replace(b'\xec\x5b', b'[ingz]')
+		content = content.replace(b'\xec\x45', b'[heart]')
+		content = content.replace(b'\xec\x50', b'[wacky len=3]')
+		content = content.replace(b'\xec\x52', b'[wacky len=6]')
+		content = content.replace(b'\xec\x53', b'[wacky len=9]')
+		content = content.replace(b'\xec\x54', b'[wacky len=12]')
+		# 处理Line宏
+		content = re.sub(b'((?:\xec\x4a)(?:\xec\x46)*(?:\xec\x48))',
+			lambda m: b'[line len=' + str(int(len(m.group(1))/2)).encode(encoding) + b']', content)
+		# 处理Block宏
+		'''
+		content = re.sub(b'(\x81\x45){2:}',
+			lambda m: b'[block len=' + str(int(len(m.group(1))/2)).encode(encoding) + b']', content)
+		'''
+		content = re.sub(b'((?:\xec\x4b)(?:\xec\x47)*(?:\xec\x49))',
+			lambda m: b'[block len=' + str(int(len(m.group(1))/2)).encode(encoding) + b']', content)
+		# 解码为unicode
+		try:
+			content = content.decode(encoding)
+		except UnicodeDecodeError:
+			print(content)
+			sys.exit(20)
+
+		# 上标文字 Ruby
+		content = re.sub('<(.*?),(.*?)>', self.get_ruby_macro, content)
+		# 换行标记 ^
+		content = re.sub('(\^+)', self.get_beginning_r_macro, content)
+		# 颜色标记 @c(r,g,b) = [font color=0x000000]
+		content = re.sub('@c\((\d+),(\d+),(\d+)\)',
+			lambda m: '[font color=0x{:0>2}{:0>2}{:0>2}]'.format(m.group(1), m.group(2), m.group(3)), content)
+		# @s(28) [large] @s(16) [small]
+		content = content.replace('@s(28)', '[large]')
+		content = content.replace('@s(16)', '[small]')
+		return content
+
+	# For re.sub
+	def get_ruby_macro(self, m):
+		ruby_macro = '[ruby text=' + m.group(2)
+		if len(m.group(1)) > 1: ruby_macro += ' char=' + str(len(m.group(1)))
+		ruby_macro += ']' + m.group(1)
+		return ruby_macro
+
+	# For re.sub
+	def get_beginning_r_macro(self, m):
+		br_count = len(m.group(1))
+		return '@r\n' * (br_count - 1) if br_count > 1 else ''
+
+# 处理剧本文件
+class ScenarioFile:
+	def __init__(self, filename):
+		self.lines = None
+		self.newlines = []
+		self.filename = filename
+		self.basename = filename.split('.')[0]
+		self.open_file()
+		self.output_file()
+
+	def open_file(self):
+		fs = open(self.filename, 'rb')
+		text = fs.read()
+		self.lines = text.split(bytes(';', encoding))
+		fs.close()
+		self.handle_file()
+
+	def handle_file(self):
+		message_handle = ScenarioMessage()
+		for line in self.lines:
+			scenario_line = ScenarioLine(line) # 处理行
+			# [hfu] [hfl]
+			if(scenario_line.macro == 'HFUL'):
+				message_handle.is_HFUL = True
+				continue
+			# 剧本特殊处理
+			if(message_handle.need_next or message_handle.temp_need_next > 0 or scenario_line.ismessage):
+				message_handle.add_line(scenario_line) # need_next 等变量可能会变化所以需要重新判断
+				if(message_handle.need_next == False and message_handle.temp_need_next == 0):
+					self.newlines.append(message_handle.newlines)
+					message_handle.reset_variables()
+				continue
+			# /剧本特殊处理
+			self.newlines.append(scenario_line.newline)
+
+	def output_file(self):
+		fs = codecs.open(self.basename+'.ks', 'w', 'u16') # 文本方式打开
+		new_file = ''
+		for line in self.newlines:
+			if not line == '': 
+				# ~ 号需要特殊处理
+				line = line.replace('〜', '～')
+				new_file += line + '\n'
+		# 去除多余的换行
+		new_file = new_file.replace('\n\n', '\n')
+		fs.write(new_file)
+		fs.close()
+
+def parse_args():
+	parser = argparse.ArgumentParser(description=__doc__)
+	parser.add_argument('input', metavar='input', help='input .ini file or folder')
+	return parser, parser.parse_args()
+
+def convert_verb(args):
+	if not os.path.exists(args.input):
+		parser.print_usage()
+		print('error: the following file or folder does not exist: ' + args.input)
+		sys.exit(20)
+
+	if os.path.isfile(args.input):
+		scenario_file = ScenarioFile(args.input)
+	else: # 文件夹
+		os.chdir(args.input)
+		for file in os.listdir('.'):
+			if file.endswith('ini'):
+				#print(file.encode('GBK', 'ignore').decode('GBK'))
+				scenario_file = ScenarioFile(file)
+
+if __name__ == '__main__':
+	parser, args = parse_args()
+	if (args.input != None): convert_verb(args)
+	else:
+		parser.print_usage()
+		sys.exit(20)
+	sys.exit(0)
